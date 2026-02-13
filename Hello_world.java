4163840f875198369278954882510c57ba41d368
Step-Sea <stepwork135@gmail.com>
2026-02-13 16:07:29 +0400

    New changes to hello world

--- Hello_world.java
+++ Hello_world.java
@@ -2,5 +2,7 @@ class Hello_world {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("This is a test of the system.");
+        System.out.print("Testing");
+        System.out.print(" without a newline.");
     }
 }
